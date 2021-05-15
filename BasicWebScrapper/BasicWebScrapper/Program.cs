4f917f0b20ab757f3dbdd909db37d3358d75bac7
stchao <chaosteve8@gmail.com>
2021-05-14 21:53:02 -0400

    Added methods to loop through all the pages and parse some of the computer anchor links to get brand, CPU, RAM, and storage.

--- BasicWebScrapper/BasicWebScrapper/Program.cs
+++ BasicWebScrapper/BasicWebScrapper/Program.cs
@@ -12,10 +12,11 @@ namespace BasicWebScrapper
 {
     class Program
     {
-        private static IConfiguration _configuration;
-        private static IHttpClientFactory _httpClientFactory;
         private static string _bestBuyURL;
+        private static DateTime startTime;
+        private static DateTime endTime;
 
+        // Private variables to hold computer possible information
         private static string[] _availableComputerBrands;
         private static string[] _availableComputerCPU;
         private static string[] _availableComputerGPU;
@@ -24,60 +25,94 @@ namespace BasicWebScrapper
 
         static void Main(string[] args)
         {
+            startTime = DateTime.Now;
             InitializeVariables();
-            GetComputersFromBestBuy();
-
+            var test = GetComputersFromBestBuy();
+            endTime = DateTime.Now;
+            Console.WriteLine(endTime - startTime);
             //Console.WriteLine("Hello World!");
         }
 
-        static void GetComputersFromBestBuy()
+        // Method that takes no parameters and scrapes best buy desktop computers
+        static List<Computer> GetComputersFromBestBuy()
         {
+            // Declare and initialize html web, and load the first page of the best buy URL
             HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlDocument doc = web.Load(_bestBuyURL);
 
-            var rightColumnClassHtmlNodes = doc.DocumentNode.SelectNodes("//div[contains(@class,'right-column')]");
+            // First page is already loaded so start at 1
+            var currentPage = 1;
 
-            // computerSpecs[1].ToList()[1].InnerText = User Rating
-            // computerSpecs[1].ToList()[0].InnerText = Openbox option
-            // Html node list that contains brand, model, cpu, gpu, ram, and storage
-            var computerInfoHtmlHodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("a").Where(node => node.GetAttributeValue("href", "").Contains("/site/"))).ToList();
+            // If "page-item" can't be found, assume there is only one page so parse "1" and assign to numberOfPages
+            Int32.TryParse(doc.DocumentNode.SelectNodes("//li[contains(@class,'page-item')]").Last().InnerText ?? "1", out int numberOfPages);
 
-            // Html node list that contains SKU and Model Number
-            var computerModelSkuHtmlNodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("sku-value"))).ToList();
+            // If number of pages equals 0, assign it the same number as currentPage otherwise assign to itself
+            numberOfPages = numberOfPages == 0 ? currentPage : numberOfPages;
 
-            // Html node list that contains the price
-            var computerPriceHtmlNodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("span").Where(node => node.GetAttributeValue("aria-hidden", "").Equals("true"))).ToList();
-
-            List <Computer> computers = new List<Computer>();
-
-            for (int i = 0; i < computerInfoHtmlHodeList.Count; i++)
+            List<Computer> computers = new List<Computer>();
+            
+            while (currentPage <= numberOfPages)
             {
-                if (computerInfoHtmlHodeList[i].ToList().Count > 0 && computerModelSkuHtmlNodeList[i].ToList().Count > 0 && computerPriceHtmlNodeList[i].ToList().Count > 0)
+                // Html nodes that contain all the computer information
+                var rightColumnClassHtmlNodes = doc.DocumentNode.SelectNodes("//div[contains(@class,'right-column')]");
+
+                // computerSpecs[1].ToList()[1].InnerText = User Rating
+                // computerSpecs[1].ToList()[0].InnerText = Openbox option
+                // Html node list that contains brand, model, cpu, gpu, ram, and storage
+                var computerInfoHtmlNodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("a").Where(node => node.GetAttributeValue("href", "").Contains("/site/"))).ToList();
+
+                // Html node list that contains SKU and Model Number
+                var computerModelSkuHtmlNodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("sku-value"))).ToList();
+
+                // Html node list that contains the prices
+                var computerPriceHtmlNodeList = rightColumnClassHtmlNodes.Select(node => node.Descendants("span").Where(node => node.GetAttributeValue("aria-hidden", "").Equals("true"))).ToList();
+
+                // Go through each of the node lists
+                for (int i = 0; i < computerInfoHtmlNodeList.Count; i++)
                 {
-                    var computerBrandAndHardware = computerInfoHtmlHodeList[i].ToList()[0].InnerText;
-                    computers.Add(new Computer()
+                    // Only try to create a computer object and add it to the list of the node lists aren't empty
+                    if (computerInfoHtmlNodeList[i].ToList().Count > 0 && computerModelSkuHtmlNodeList[i].ToList().Count > 0 && computerPriceHtmlNodeList[i].ToList().Count > 0)
                     {
-                        Title = computerInfoHtmlHodeList[i].ToList()[0].InnerText
-                        Brand = "",
-                        Model = "",
-                        CPU = "",
-                        GPU = "",
-                        RAM = "",
-                        Storage = "",
-                        ModelNumber = computerModelSkuHtmlNodeList[i].ToList()[0].InnerText,
-                        SKU = computerModelSkuHtmlNodeList[i].ToList()[1].InnerText,
-                        Cost = computerPriceHtmlNodeList[i].ToList()[1].InnerText,
-                        Link = computerInfoHtmlHodeList[i].ToList()[0].Attributes["href"].Value,
-                    });
-                }                
+                        var computerBrandAndHardware = computerInfoHtmlNodeList[i].ToList()[0].InnerText;
+                        computers.Add(new Computer()
+                        {
+                            // include title in case string parse goes funny
+                            Title = computerBrandAndHardware,
+                            Brand = getComputerInformation(computerBrandAndHardware, _availableComputerBrands),
+                            Model = "",
+                            CPU = getComputerInformation(computerBrandAndHardware, _availableComputerCPU),
+                            GPU = getComputerInformation(computerBrandAndHardware, _availableComputerGPU),
+                            RAM = getComputerInformation(computerBrandAndHardware, _availableComputerRAM),
+                            Storage = getComputerInformation(computerBrandAndHardware, _availableComputerStorage),
+                            ModelNumber = computerModelSkuHtmlNodeList[i].ToList()[0].InnerText,
+                            SKU = computerModelSkuHtmlNodeList[i].ToList()[1].InnerText,
+                            Cost = computerPriceHtmlNodeList[i].ToList()[1].InnerText,
+                            Link = computerInfoHtmlNodeList[i].ToList()[0].Attributes["href"].Value,
+                        });
+                    }
+                }
+
+                // Increment page and assign the next loaded page to doc
+                currentPage++;
+                doc = web.Load("https://www.bestbuy.com/site/desktop-computers/all-desktops/pcmcat143400050013.c?cp=" + currentPage + "id=pcmcat143400050013");
             }
+
+            return computers;
         }
 
+        // Method to check if any items in the given array of strings is in the given string, and returns "N/A" if it can't be found
+        static string getComputerInformation(string stringToSearch, string[] listOfInfo)
+        {
+            for (int i = 0; i < listOfInfo.Length; i++)
+            {
+                if (stringToSearch.Contains(listOfInfo[i])) { return listOfInfo[i]; }
+            }
+            return "N/A";
+        }
+
+        // Method to initialize private variables
         static void InitializeVariables()
         {
-            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
-            _httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
-
             _bestBuyURL = "https://www.bestbuy.com/site/desktop-computers/all-desktops/pcmcat143400050013.c?id=pcmcat143400050013";
 
             _availableComputerBrands = new string[] { "Acer", "Alienware", "Apple", "ASUS", "Azulle", "CanaKit", "CLX", "CORSAIR", "CyberPowerPC", 
@@ -91,13 +126,12 @@ namespace BasicWebScrapper
             _availableComputerStorage = new string[] { "64GB", "128GB", "256GB", "480GB", "512GB", "1TB", "2TB" };
         }
 
+        // Method to get app settings information
         static void GetConfiguration()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
-
-            _configuration = builder.Build();
         }
     }
 }
--- BasicWebScrapper/README.txt
+++ BasicWebScrapper/README.txt
@@ -1,4 +1,10 @@
 Current Version 0.0.0
 
 Change Log:
-Version 0.0.0: Basic web scrapper utilizing HtmlAgilityPack to get the first page of BestBuy's desktop computers
+Version 0.0.1: 
+- Added XPath expression to get last page number and a loop method to go through all the available pages of BestBuy's desktop computers
+- Added for loop method, with a worst case run time of O(N), to parse the inner texts of anchor links that have computer information to extract brand, CPU, RAM, and storage. Parsing does not include GPU or Model.
+- Average run time is 7:23.7669286 (first: 7:22.7933780; second: 7:24.6682119; third: 7:23.8391959)
+
+Version 0.0.0: Basic web scrapper utilizing HtmlAgilityPack and XPath expressions to get the computer information listed on the first page of BestBuy's desktop computers
+
